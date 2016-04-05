9fdf6d4b4288ecbc320955a74a01dd13381d6fe5
buxiaoyang <buxiaoyang@gmail.com>
2016-04-05 21:13:32 +0800

    add time delay to control main while run time to about 10ms.

--- Firmware/source/main.c
+++ Firmware/source/main.c
@@ -18,30 +18,6 @@
 #include <key.h>
 #include <dispatch.h>
 
-void getSensorStatus()
-{
-	/*
-	//初始位置传感器1
-	if(sensorStartPosi1 == 0)
-	{
-		motor1.isStartPosition = 1;
-	}
-	if(sensorStartPosi1 == 1)
-	{
-		motor1.isStartPosition = 0;
-	}
-	//初始位置传感器2
-	if(sensorStartPosi2 == 0)
-	{
-		motor2.isStartPosition = 1;
-	}
-	if(sensorStartPosi2 == 1)
-	{
-		motor2.isStartPosition = 0;
-	}
-	*/
-}
-
 /***************************************************************************/
 // 主函数
 // 参数：无
@@ -54,9 +30,28 @@ void main()
 	uart_init();
 	Timer0Init();
 	Timer1Init();
-	ChangeScreenPage(0x00);
+	ChangeScreenPage(0x00);	 
 	while(1)
 	{
+		//控制主循环执行时间为10ms
+		if( motor1.status != MOTOR_STOP && motor2.status != MOTOR_STOP)
+		{
+			//两个电机一起运作
+			delay_ms(4);
+		}
+		else if(motor1.status != MOTOR_STOP || motor2.status != MOTOR_STOP)
+		{
+			//只有一个电机运作
+			delay_ms(6);
+		}
+		else
+		{
+			//没有电机运作
+			delay_ms(10);
+		}
+		//输出测试信号
+		testOutput1 = !testOutput1;
+
 		//电机初始化状态机
 		ResetMotorDispatch();
 		//刷新显示器
@@ -86,74 +81,6 @@ void main()
 			}
 			saveMode = SAVE_NO_SAVING;
 		}
-		/*
-		delay_us(100);
-		//按键扫描
-		count10ms++;
-		if(count10ms > 29)
-		{
-			Key_Scan();
-			count10ms = 0;
-		}
-		//General the PWM
-		if(pulseSettingNumCount > 20)
-		{
-			timer_count --;
-		}
-		else
-		{
-			timer_count ++;
-		}
-		if(timer_count < 1)
-		{
-			timer_count = 1;	
-		}
-		else if(timer_count > 50)
-		{
-			timer_count = 50;
-		}
-		//Get the sensor status
-		getSensorStatus();
-		if(isStartPosition2 || isStartPosition1)
-		{
-			isStartPosition = 1;
-		}
-		else if(!isStartPosition2 && !isStartPosition1)
-		{
-			isStartPosition = 0;
-		}
-		//初始化过程中判断原点
-		if(initFlag == 1 && isStartPosition == 1)
-		{
-			pulseSettingNumCount = 4;
-			currentPosition = 1;
-			initFlag = 0;
-		}
-		//Save setting to eeprom
-		if(saveSetting)
-		{
-			//计算电机旋转角
-			temp = (unsigned long)pulseSettingNum*motorStepAngle;
-			motorRotationAngle = temp/motorReducGearRatio;
-			//计算丝杆导程
-			temp = (unsigned long)pulseSettingNum*motorStepAngle;
-			temp = 	(unsigned long)temp * screwPitch;
-			temp = 	(unsigned long)temp / motorReducGearRatio;
-			ballScrew = temp/3600;
-			//保存设置
-			ChangeScreenPage(0x04);
-			parameter_save();
-			ChangeScreenPage(0x02);
-			saveSetting = 0;
-			refreshDisplay = 1;
-		}
-		//刷新显示	
-		if(refreshDisplay)
-		{
-			parameter_send_screen();
-			refreshDisplay = 0;
-		}
-		*/
 	}   
 
 }
